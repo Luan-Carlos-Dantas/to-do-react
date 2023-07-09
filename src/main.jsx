705e69eac8f06f07a46c2f778613c3ef7fa150da
Luan-Carlos-Dantas <luancarlos492@gmail.com>
2023-07-09 18:56:50 -0300

    Fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './global.css';
+import './styles/global.css';
 import App from './components/App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
